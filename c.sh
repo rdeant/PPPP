65a13210f418838df2c264c631620257d36af243
rdt25097 <rdt25097@uga.edu>
2024-05-16 14:52:59 -0400

    working with output file up to 100k

--- c.sh
+++ c.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -ex
 javac -d bin src/rdt/PPInfo.java
 javac -d bin -cp bin src/rdt/Gen.java
+javac -d bin -cp bin src/rdt/Driver.java
---
+++ src/rdt/Driver.java
@@ -0,0 +1,61 @@
+package rdt;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import rdt.PPInfo;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("./output.txt");
+            Scanner s = new Scanner(file);
+            Scanner kb = new Scanner(System.in);
+            System.out.println("Enter a number to find the smallest integer with exactly 2^n divisors");
+            int input = kb.nextInt();
+            int index = 0;
+            ArrayList<PPInfo> build = new ArrayList<PPInfo>();
+            while (index < input && s.hasNextLine()) {
+                index++;
+                String str = s.nextLine();
+                PPInfo temp = new PPInfo(str);
+                if (temp.power != 1) {
+                    for (PPInfo x : build) {
+                        if (temp.prime == x.prime) {
+                            x.power += temp.power;
+                        }
+                    }
+                } else {
+                    build.add(temp);
+                }
+            }
+            kb.close();
+            s.close();
+            int beg = 0;
+            int pow = build.get(0).power;
+            for (int i = 1; i < build.size(); i++) {
+                if (build.get(i).power != pow) {
+                    if (i - beg == 1) {
+                        System.out.println(build.get(beg));
+                    } else {
+                        System.out.println("(" + build.get(beg).prime + "..."
+                        + build.get(i - 1).prime + ")^" + pow);
+                    }
+                    beg = i;
+                    pow = build.get(i).power;
+                } else if (i == build.size() - 1) {
+                    System.out.print("(" + build.get(beg).prime + "..."
+                    + build.get(i).prime + ")");
+                    if (build.get(i).power != 1) {
+                        System.out.print("^" + build.get(i).power);
+                    }
+                    System.out.println();
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
